29b7012df4965cc100357c7e722cc97eec5dfff4
Frank-Proulx <frankproulx7@gmail.com>
2021-12-06 10:14:33 -0800

    add tests and logic for ping, pong and ping-pong
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -4,8 +4,16 @@ def ping_pong(number)
   array = []
   counter = 0
   while (counter < number)
-  counter += 1
-  array.push(counter)
+    counter += 1
+    if (counter.%(3).eql?(0)) && (counter.%(5).eql?(0))
+      array.push('ping-pong')
+    elsif counter.%(3).eql?(0)
+      array.push("ping")
+    elsif counter.%(5).eql?(0)
+      array.push("pong")
+    else
+      array.push(counter)
+    end
   end
   array
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -2,9 +2,8 @@ require('rspec')
 require('ping_pong')
 
 describe('#ping_pong') do
-  it("returns an array of ascending numbers up to the number entered") do
-    expect(ping_pong(15)).to(eq([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
+  it("returns an array of ascending numbers up to the number entered, replacing any number divisible by 3 with ping, and any numbers divisble by 5 with pong, and any divisble by 3 and 5 with ping-pong") do
+    expect(ping_pong(15)).to(eq([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"ping-pong"]))
   end
-
 
 end
