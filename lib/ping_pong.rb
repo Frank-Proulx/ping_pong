2b49a30b3c08f9c4e41f686b0e370b26207fbd0f
Frank-Proulx <frankproulx7@gmail.com>
2021-12-06 09:46:12 -0800

    pass first test counting up to the inputted number
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,1 +1,11 @@
 require('pry')
+
+def ping_pong(number)
+  array = []
+  counter = 0
+  while (counter < number)
+  counter += 1
+  array.push(counter)
+  end
+  array
+end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -1,2 +1,10 @@
 require('rspec')
 require('ping_pong')
+
+describe('#ping_pong') do
+  it("returns an array of ascending numbers up to the number entered") do
+    expect(ping_pong(15)).to(eq([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
+  end
+
+
+end
